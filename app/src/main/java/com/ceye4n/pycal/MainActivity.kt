dd3c641a69f6577465c3f9e2f8bff55ccbb60976
Ceye4n <129878378+Ceye4n@users.noreply.github.com>
2023-11-16 00:17:59 +0100

    commit

--- app/src/main/java/com/ceye4n/pycal/MainActivity.kt
+++ app/src/main/java/com/ceye4n/pycal/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -38,6 +39,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CodeInput() {
-    var text by remember { mutableStateOf("...") }
-    TextField(value = text, onValueChange = { text = it}, label = { Text("CODE") })
+    var input by remember { mutableStateOf("") }
+    TextField(value = input, onValueChange = { input = it}, label = { Text("CODE") })
+}
+
+@Composable
+fun StringResourceText() {
+    Text(stringResource(R.string.CodeInput.input))
 }
