e5989635bc3091e6273970eff4b87f5bdc61a20d
Ceye4n <129878378+Ceye4n@users.noreply.github.com>
2023-11-15 15:53:49 +0100

    commit

--- app/src/main/java/com/ceye4n/pycal/MainActivity.kt
+++ app/src/main/java/com/ceye4n/pycal/MainActivity.kt
@@ -7,8 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.ceye4n.pycal.ui.theme.PyCalTheme
 
@@ -22,7 +28,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
+                    CodeInput()
                 }
             }
         }
@@ -30,17 +37,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PyCalTheme {
-        Greeting("Android")
-    }
+fun CodeInput() {
+    var text by remember { mutableStateOf("...") }
+    TextField(value = text, onValueChange = { text = it}, label = { Text("CODE") })
 }
